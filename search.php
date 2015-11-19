c6870cb577402708bf17dfac5f43758be821a10d
flyingrican <kortiz1514@gmail.com>
2015-11-19 01:38:23 -0500

    Update search.php

--- search.php
+++ search.php
@@ -1,9 +1,35 @@
 <?php 
 
+include "functions.php";
+
 $dbc = mysqli_connect('localhost', 'root', 'knight15', 'ajax');
 
 $search = $_POST['search'];
 
-echo $search;
+if (!empty($search)) {
+	
+	$query = "SELECT * FROM cars WHERE cars LIKE '$search%'";
+	$search_query = mysqli_query($dbc, $query);
+
+	confirm($search_query);
+
+	while ($row = mysqli_fetch_array($search_query)) {
+		
+		$brand = $row['cars'];
+		?>
+
+		<ul class="list-unstyled">
+		<?php
+
+			echo "<li>{$brand} in Stock</li>";
+
+
+		?>
+		</ul>
+
+
+<?php	}
+
+}
 
 ?>

b591880892ff26713c34196b24c5213a6b73a193
flyingrican <kortiz1514@gmail.com>
2015-11-19 02:16:29 -0500

    Update add_cars.php

--- add_cars.php
+++ add_cars.php
@@ -3,5 +3,17 @@
 include "db.php";
 include 'functions.php';
 
+if (isset($_POST['car_name'])) {
+	
+	$car_name = $_POST['car_name'];
+
+	$query = "INSERT INTO cars(cars) VALUES('$car_name')";
+	$query_car_name = mysqli_query($dbc, $query);
+	confirm($query_car_name);
+
+}
+
+header("Location: index.html");
+
 
 ?>
